b82cc9529dd892301557e6e873fd8e8e789c7d54
Ishan <91502698+0x26shanx@users.noreply.github.com>
2023-05-22 00:52:18 +0530

    Update exifupdater.py
    Initializing the ExifToolHelper instance only once and reusing it for all the image files

--- exifupdater.py
+++ exifupdater.py
@@ -1,19 +1,18 @@
 from exiftool import ExifToolHelper
 
-def exifupdater(allfiles,datemeta):
-
-    for i,j in zip(allfiles,datemeta):
-        with ExifToolHelper() as et:
-            et.set_tags(
-            i,
-            tags={
-                'FileCreateDate':  j,
-                'DateTimeOriginal':  j,
-                'FileModifyDate':  j},
-            params=["-P", "-overwrite_original"]
-        )
-
+def exifupdater(allfiles, datemeta):
     with ExifToolHelper() as et:
-        for d in et.get_tags(allfiles,tags=['FileCreateDate','FileModifyDate','DateTimeOriginal']):
+        for i, j in zip(allfiles, datemeta):
+            et.set_tags(
+                i,
+                tags={
+                    'FileCreateDate': j,
+                    'DateTimeOriginal': j,
+                    'FileModifyDate': j
+                },
+                params=["-P", "-overwrite_original"]
+            )
+
+        for d in et.get_tags(allfiles, tags=['FileCreateDate', 'FileModifyDate', 'DateTimeOriginal']):
             for k, v in d.items():
                 print(f"{k} = {v}")

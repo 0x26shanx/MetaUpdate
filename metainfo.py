62f44f0585f1f3aced5a6007150a9633db3fb1d7
Ishan <91502698+0x26shanx@users.noreply.github.com>
2023-05-22 00:54:07 +0530

    Update metainfo.py
    Implemented regular expressions

--- metainfo.py
+++ metainfo.py
@@ -1,40 +1,31 @@
 import datetime
 import os
+import re
 
 def metainfo(path):
-    str = os.listdir(path)
+    files = os.listdir(path)
     ext = ('.jpeg','.jpg','.png','.JPEG','.JPG','.PNG')
     
     image_list = []
     datearray = []
     
-    for j in str:
-        digit=0
-        for ch in j:
-            if ch.isdigit():
-                digit=digit+1
-        if digit >= 14:
-            if j.endswith(ext):
-                image_list.append(j)
-
-    for i in image_list:
-        for tp in ext:
-            i = i.replace(tp,'')
-        i = i.split("_")
-        for splitted in i:
-            if splitted.startswith("20") == True:
-                if len(splitted) == 8:
-                    splitted = splitted[:8]
-                    date = datetime.datetime.strptime(splitted, "%Y%m%d-%H%M%S")
+    for filename in files:
+        if filename.endswith(ext):
+            match = re.search(r'\d{8}[-_]\d{6}', filename)
+            if match:
+                date = datetime.datetime.strptime(match.group(), "%Y%m%d_%H%M%S" if '_' in match.group() else "%Y%m%d-%H%M%S")
+                date = date.strftime("%Y:%m:%d %H:%M:%S")
+                image_list.append(filename)
+                datearray.append(date)
+            else:
+                match = re.search(r'\d{8}', filename)
+                if match:
+                    date = datetime.datetime.strptime(match.group(), "%Y%m%d")
                     date = date.strftime("%Y:%m:%d 12:00:00")
+                    image_list.append(filename)
                     datearray.append(date)
-                elif len(splitted) == 19:
-                    splitted = splitted[:19]
-                    date = datetime.datetime.strptime(splitted, "%Y-%m-%d_%H-%M-%S")
-                    date = date.strftime("%Y:%m:%d %H:%M:%S")
-                    datearray.append(date)
+    
     return image_list,datearray
-
 
     # for i in str:
     #     if i.endswith(ext):
